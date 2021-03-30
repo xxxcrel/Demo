25fc91771152ad45507b2cf56c23a31985f7a427
ShtermCrel <wuxc@shterm.com>
2021-03-30 16:44:47 +0800

    perf: specify grpc generated dir

---
+++ src/main/java/beer/cheese/netty/EventLoopDemo.java
@@ -0,0 +1,14 @@
+package beer.cheese.netty;
+
+import java.util.concurrent.TimeUnit;
+
+import io.netty.channel.Channel;
+import io.netty.channel.embedded.EmbeddedChannel;
+
+public class EventLoopDemo {
+
+    public static void main(String[] args) {
+        Channel channel = new EmbeddedChannel();
+        channel.eventLoop().schedule(() -> {System.out.println("hel");}, 1000L, TimeUnit.MILLISECONDS);
+    }
+}
--- src/main/kotlin/beer.cheese/Hello.kt
+++ src/main/kotlin/beer.cheese/Hello.kt
@@ -1,3 +1,8 @@
 fun main(){
     println("hello kotlin")
+    hello("wuxc", 23)
+}
+
+fun hello(name: String, age: Int){
+    println("name${name}, age: ${age}")
 }
