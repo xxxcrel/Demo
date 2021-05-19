f3b987fb56152317ccdeb54257d23461eea2d001
xxxcrel <crelxc@gmail.com>
2021-05-19 09:18:06 +0800

    Add Spring Cloud Demo

---
+++ src/main/java/beer/cheese/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package beer.cheese.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void init(WebSecurity web) throws Exception {
+        super.init(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+}
