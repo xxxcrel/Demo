2e4f4b8119be63396448175286e561822b05610a
xxxcrel <crelxc@gmail.com>
2021-05-28 09:08:37 +0800

    Update: Jackson demo

--- src/main/java/beer/cheese/springboot/CheeseProperties.java
+++ src/main/java/beer/cheese/springboot/CheeseProperties.java
@@ -1,5 +1,7 @@
 package beer.cheese.springboot;
 
+import java.util.Map;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "beer.cheese")
@@ -7,11 +9,21 @@ public class CheeseProperties {
 
     String appName;
 
+    Map<String, String> map;
+
     public String getAppName() {
         return appName;
     }
 
     public void setAppName(String appName) {
         this.appName = appName;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
     }
 }
