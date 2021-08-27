81903315f2e5c79d7496485b3062cff9123b0b4a
xxxcrel <crelxc@gmail.com>
2021-08-27 15:17:49 +0800

    Add: thymeleaf demo

---
+++ src/main/java/cn/qisee/thymeleaf/ThymeleafDemo.java
@@ -0,0 +1,70 @@
+package cn.qisee.thymeleaf;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Locale;
+
+import javax.swing.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.W3CDom;
+import org.jsoup.nodes.Document;
+import org.springframework.core.io.ClassPathResource;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+
+public class ThymeleafDemo extends JFrame {
+    public static void main(String[] args) throws IOException {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("template/");
+        resolver.setTemplateMode(TemplateMode.JAVASCRIPT);
+        resolver.setSuffix(".html");
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(resolver);
+        Context context = new Context(Locale.CHINA);
+
+        context.setVariable("name", "wuxc");
+        context.setVariable("week", new String[]{"Mon", "Tue", "Web", "Thu", "Fri"});
+        context.setVariable("data", new int[]{10, 20, 18, 30, 28});
+        String resolvedHtml = engine.process("index", context);
+        ClassPathResource echartsFile = new ClassPathResource("/template/echarts.min.js");
+        String echarts = new String(Files.readAllBytes(echartsFile.getFile().toPath()), StandardCharsets.UTF_8);
+        String finalHtml = resolvedHtml.replace("###echartsjs###", echarts);
+        System.out.println(resolvedHtml);
+        Files.write(Paths.get(System.getProperty("user.home") + "/index.html"), finalHtml.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
+//        ScriptEngine jsEngine = new ScriptEngineManager().getEngineByName("JavaScript");
+//        try {
+//            jsEngine.eval(finalHtml);
+//        } catch (ScriptException e) {
+//            e.printStackTrace();
+//        }
+
+        Document doc;
+
+        doc = Jsoup.parse(finalHtml);
+        try (OutputStream os = new FileOutputStream("/Users/wuxc/index.pdf")) {
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.useFastMode();
+            builder.withW3cDocument(new W3CDom().fromJsoup(doc), null);
+            builder.toStream(os);
+            builder.run();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
