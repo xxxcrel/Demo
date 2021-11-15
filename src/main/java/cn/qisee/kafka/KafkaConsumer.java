bc64eb796bb6ce8fda4b4396542c6edd0dc28c23
xxxcrel <crelxc@gmail.com>
2021-11-15 21:34:30 +0800

    Add Kafka demo

---
+++ src/main/java/cn/qisee/kafka/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package cn.qisee.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = {"kafka_test"}, groupId = "1")
+    public void receive(ConsumerRecord<String, String> data) {
+        System.out.println(data);
+    }
+}
