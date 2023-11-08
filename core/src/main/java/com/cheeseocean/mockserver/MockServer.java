d475dcc3d53ff1680e6a326e7a2cb24b3082a4e9
xxxcrel <xxxcrel@gmail.com>
2023-11-08 11:07:39 +0800

    chore: rename project

--- core/src/main/java/com/cheeseocean/mockserver/MockServer.java
+++ core/src/main/java/com/cheeseocean/mockserver/MockServer.java
@@ -1,22 +1,16 @@
 package com.cheeseocean.mockserver;
 
-import java.util.Map;
-
 import lombok.Data;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -36,18 +30,18 @@ public class MockServer {
 //    }
 
     @GetMapping("/noResponse")
-    public void noResponse(){
+    public void noResponse() {
         logger.info("/noResponse");
     }
 
     @PostMapping("/submitMap")
-    public Map submitMap(@RequestParam Map<String , String> form){
+    public Map submitMap(@RequestParam Map<String, String> form) {
         logger.info(form);
         return form;
     }
 
     @PostMapping("/json")
-    public Map json(@RequestBody Map<String, String> json){
+    public Map json(@RequestBody Map<String, String> json) {
         logger.info(json);
         return json;
     }
