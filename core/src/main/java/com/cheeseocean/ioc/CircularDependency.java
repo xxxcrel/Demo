e99bc9cd101499e0a079fedcab63cc3c788b4313
xxxcrel <xxxcrel@gmail.com>
2023-11-08 14:44:10 +0800

    Update ioc circular dependency with aop

--- core/src/main/java/com/cheeseocean/ioc/CircularDependency.java
+++ core/src/main/java/com/cheeseocean/ioc/CircularDependency.java
@@ -1,38 +1,56 @@
 package com.cheeseocean.ioc;
 
 import lombok.Getter;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackageClasses = {A.class, B.class})
+@EnableAspectJAutoProxy
 public class CircularDependency {
 
     public static void main(String[] args) {
         System.setProperty("test.a", "/sysenv/");
         ApplicationContext context = new AnnotationConfigApplicationContext(CircularDependency.class);
-        C c = context.getBean(C.class);
-        System.out.println(c.getB());
-//        A a = context.getBean(A.class);
-//        B b = a.takeB();
-//        Assert.state(a.equals(b.takeA()), "Not equals");
+        A a = context.getBean(A.class);
+        B b = a.takeB();
+        System.out.println(b);
+        System.out.println(a.b);
+        System.out.println(a.getB());
+//        B b = new B();
+//        AProxy aProxy = new AProxy(b);
+//        A a = ((A) aProxy);
+//        System.out.println(a.b);
+    }
+}
+
+@Component
+@Aspect
+class Point {
+    @Pointcut("execution(* com.cheeseocean.ioc.A.takeB(..))")
+    void cut() {
+    }
+
+    @Before("cut()")
+    void before() {
+        System.out.println("before");
     }
 }
 
 class Constants {
     @Getter
     private static String C = "/home/";
-
 }
 
-@Component
+//@Component
 class C {
     @Value("#{T(com.cheeseocean.ioc.Constants).getC() + '${test.a:/1,/2,/3}'.replaceAll(',', ',' + T(com.cheeseocean.ioc.Constants).getC()).split(',')}}")
     private String[] a;
@@ -50,20 +68,39 @@ class C {
 }
 
 @Component
-class A{
-
+class A {
     @Autowired
     B b;
-    public B takeB(){
+
+    public B takeB() {
+        System.out.println("takeB");
         return b;
+    }
+    public B getB() {
+        return b;
+    }
+}
+class AProxy extends A {
+    private A targetSource;
+    public AProxy(A targetSource) {
+        this.targetSource = targetSource;
+    }
+    @Override
+    public B takeB() {
+        return targetSource.getB();
     }
 }
 
 @Component
-class B{
+class B {
     @Autowired
     A a;
-    public A takeA(){
+
+    public A takeA() {
         return a;
+    }
+
+    void sayHello() {
+        System.out.println("B: sayHello");
     }
 }
